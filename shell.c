01d33f74a28f736064b052736c50be2609b2d60e
Ancelin-31 <ancelin.chevallier@gmail.com>
2025-04-22 15:05:38 +0200

    add if read < 0 condtion

--- shell.c
+++ shell.c
@@ -10,36 +10,43 @@ int main(void)
 	char *line = NULL;
 	char *args[] = {NULL, NULL};
 	size_t len = 0;
+	ssize_t read = 0;
 	pid_t process = 0;
 	int status;
 	struct stat st;
 
-		while (1)
+	while (1)
+	{
+		read = getline(&line, &len, stdin);
+		if (read < 0)
+			break;
+
+		line[strlen(line) - 1] = '\0';
+		args[0] = line;
+		if (isatty(STDIN_FILENO) != 0)
+			printf("$ ");
+			
+		if (stat(args[0], &st) != 0)
+			fprintf(stderr, "hsh: 1: %s: not found\n", args[0]);
+		process = fork();
+			
+		if (process < 0)
 		{
-			getline(&line, &len, stdin);
-			line[strlen(line) - 1] = '\0';
-			args[0] = line;
-			
-			if (stat(args[0], &st) != 0)
-				fprintf(stderr, "hsh: 1: %s: not found\n", args[0]);
-			process = fork();
-			
-			if (process < 0)
-			{
-				perror("fork failed");
-				return (1);
-			}
-
-			if (process == 0)
-			{
-				execve(line, args, NULL);
-				break;
-			}
-
-			else
-				wait (&status);
-
-			free(line);
+			perror("fork failed");
+			return (1);
 		}
-	return (0);
+
+		if (process == 0)
+		{
+			execve(line, args, NULL);
+			break;
+		}
+
+		else
+			wait (&status);
+
+	
+	}
+	free(line);
+return (0);
 }
