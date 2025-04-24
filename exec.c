0086bcb16c2c7776a44632df2d9d82cf6433e833
Ancelin-31 <ancelin.chevallier@gmail.com>
2025-04-24 22:58:05 +0200

    create exec.c file

---
+++ exec.c
@@ -0,0 +1,35 @@
+#include "shell.h"
+
+/**
+ * handle_command - handles command execution
+ * @args: tokenized input
+ * @argv: program name for error message
+ * @env: environment variables
+ */
+
+void handle_command(char **args, char **argv, char **env)
+{
+	pid_t pid;
+	char *cmd_path = find_path(args[0], env);
+	int status;
+	
+	if (cmd_path == NULL)
+	{
+		fprintf(stderr, "%s: 1: %s: not found\n", argv[0], args[0]);
+		return;
+	}	pid = fork();
+	if (pid < 0)
+	{
+		perror("fork failed");
+		free(cmd_path);
+		return;
+	}
+	if (pid == 0)
+	{
+		execve(cmd_path, args, env);
+		perror("execve failed");
+		exit(EXIT_FAILURE);
+	}
+	waitpid(pid, &status, 0);
+	free(cmd_path);
+}
--- shell.c
+++ shell.c
@@ -1,69 +1,39 @@
 #include "shell.h"
 
 /**
- * main - interprets commands
- * @argc: number of arguments in the command
- * @argv: text composing the command
- * Return: 0 if success
+ * main - simple shell interpreter
+ * @argc: argument count
+ * @argv: argument values
+ * @env: environment variables
+ * Return: 0 on success
  */
 
 int main(int argc, char **argv, char **env)
 {
-	char *line = NULL, **args = NULL, *cmd_path = NULL;
-	size_t linecap;
+	char *line = NULL, **args = NULL;
+	size_t linecap = 0;
 	ssize_t lineread;
-	pid_t pid;
-	int status, i = 0;
-	(void)argc;  /* on n’utilise pas argc directement */
-	linecap = 0;
+	int i;
+	(void)argc;
 	
 	while (1)
 	{
 		if (isatty(STDIN_FILENO))
-		{
-			printf("($)");
-		}
+			printf("($) ");
+		
 		lineread = getline(&line, &linecap, stdin);
+		
 		if (lineread < 0)
-		{
 			break;
-		}
 		args = tokenize(line);
 		
-		if (args[0] == NULL)
-		{
-			while (args[i] != NULL)
-				free(args[i++]);
-			free(args);
-			free(line);
-			continue;
-		}
-		cmd_path = find_path(args[0], env);
+		if (args && args[0])
+			handle_command(args, argv, env);
 		
-		if (cmd_path == NULL)
-		{
-			fprintf(stderr, "%s: 1: %s: not found\n", argv[0], args[0]);
-			continue;
-		}
-		pid = fork();
-		if (pid < 0)
-		{
-			perror("fork failed");
-			free(cmd_path);
-			continue;
-		}
-		if (pid == 0)
-		{
-			execve(cmd_path, args, env);
-			perror("execve failed");
-			exit(EXIT_FAILURE);
-		}
-		waitpid(pid, &status, 0);
-		free(cmd_path);
+		for (i = 0; args && args[i]; i++)
+			free(args[i]);
+		free(args);
 	}
-	while (args[i])
-		free(args[i++]);
-	free(args);
 	free(line);
 	return (0);
 }
