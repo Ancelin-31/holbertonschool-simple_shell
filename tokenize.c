b2c8c078e751133d94304fcc2a3b4e331f9b49e5
Ancelin-31 <ancelin.chevallier@gmail.com>
2025-04-24 15:56:05 +0200

    memory leaks

--- tokenize.c
+++ tokenize.c
@@ -5,8 +5,20 @@ char **tokenize(char *line)
 	char **args = malloc(1024 * sizeof(char));
 	char *newLine = strdup(line);
 	char *linetok = strtok(newLine, " \n");
-	int i = 0;
+	int i = 0, j = 0;
 	
+	if (args == NULL)
+	{
+		return (NULL);
+	}
+	
+	if (newLine == NULL)
+	{
+		for (j = 0; args[j] != NULL; j++)
+			free(args[j]);
+		free(args);
+		return (NULL);
+	}
 	while (linetok != NULL)
 	{
 		args[i] = strdup(linetok);
